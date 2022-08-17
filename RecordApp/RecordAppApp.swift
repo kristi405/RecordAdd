a348794179a6d28442d7daa8d6c4b95726105bcf
Kristi <5507151kr@gmail.com>
2022-08-17 15:59:12 +0200

    Initial Commit

---
+++ RecordApp/RecordAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordAppApp.swift
+//  RecordApp
+//
+//  Created by Kristina Makarevich on 17.08.22.
+//
+
+import SwiftUI
+
+@main
+struct RecordAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
